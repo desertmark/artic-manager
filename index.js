14765f26e780977387dcb0f50e8e09e5af64d880
Fernando Gabriel Asulay Carnevale <fernando.asulay@globant.com>
2018-07-04 16:42:12 -0300

    comment all lines from index to test deploy

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const PORT_NUMBER       = 3000; 
 const info              = `App is running on port: ${PORT_NUMBER}`;
 const app               = require('express')();
-const passport          = require('passport');
-const configurePassport = require('./config/passport');
-const awilixConfig      = require('./config/awilix');
-const awilixExpress     = require('awilix-express');
-const bodyParser        = require('body-parser');
-require('./data/seed-data')();
+// const passport          = require('passport');
+// const configurePassport = require('./config/passport');
+// const awilixConfig      = require('./config/awilix');
+// const awilixExpress     = require('awilix-express');
+// const bodyParser        = require('body-parser');
+// require('./data/seed-data')();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-// AWILIX
-const container = awilixConfig.getContainer();
-app.use(awilixConfig.scopeContainer(container));
+// // AWILIX
+// const container = awilixConfig.getContainer();
+// app.use(awilixConfig.scopeContainer(container));
 
-//PASSPORT
-configurePassport(passport, container);
-app.use(passport.initialize());
-app.use(passport.session());
-// ROUTERS
-app.use(awilixExpress.loadControllers('**/*-router.js'))
-const articlesRouter = require('./articles');
-app.use('/', articlesRouter); // Loading articles router separately because it uses router.param which is not expose by awilix's createController wrapper.
+// //PASSPORT
+// configurePassport(passport, container);
+// app.use(passport.initialize());
+// app.use(passport.session());
+// // ROUTERS
+// app.use(awilixExpress.loadControllers('**/*-router.js'))
+// const articlesRouter = require('./articles');
+// app.use('/', articlesRouter); // Loading articles router separately because it uses router.param which is not expose by awilix's createController wrapper.
 
 app.get('/info', (req, res) => res.send(info));
 app.listen(PORT_NUMBER, () => console.log(info));
