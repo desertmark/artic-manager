ff4d27dc6a5da85a69aa2a64a017457736b371c6
Fernando Asulay <fernando.asulay@globant.com>
2018-07-07 12:31:45 -0300

    Virtual Cost Price and CardPrice

--- articles/articles.js
+++ articles/articles.js
@@ -30,14 +30,18 @@ const articleSchema = new Schema({
 });
 // NOTE: Don't use Arrow function, it causes "this" keyword to be equal to "{}" insetead of the model.
 // NOTE: If we take out one of these colums using projection, the value here will be null.
-articleSchema.virtual('price').get(function() {
+articleSchema.virtual('cost').get(function() {
     const totalDiscount = _.sumBy(this.discounts, 'amount');
-    const price = this.listPrice*(1 + this.utility + this.transport - totalDiscount).toFixed(2);
+    const price = this.listPrice*(1 + this.vat - totalDiscount).toFixed(2);
     return parseFloat(price);
 });
 
+articleSchema.virtual('price').get(function() {
+    return this.cost*(1 + this.utility + this.transport);
+});
+
 articleSchema.virtual('cardPrice').get(function() {
-    return this.price*(1+this.card);
+    return this.price*(1 + this.card);
 });
 
 const Article = mongoose.model('Article', articleSchema);
