e2f05e50a6840956f017a39a0e7623b596d8d1cc
Fernando Asulay <fer.guitar.ln@gmail.com>
2018-07-29 21:15:33 -0300

    Fixed articles service test

--- src/articles/specs/articles-service.spec.js
+++ src/articles/specs/articles-service.spec.js
@@ -3,18 +3,21 @@ const ArticleService = require('../articles-service.js');
 const Article = require('../articles.js');
 describe('Articles Service', () => {
     beforeEach(() => {
-        this.article = new Article();
-        this.spyRepo = jasmine.createSpyObj('articleRepository',['findById']);
-
-        this.service = new ArticleService({articleRepository: this.spyRepo});
+        console.log('before each');
+        article = new Article();
+        spyRepo = jasmine.createSpyObj('articleRepository',['findById']);
+        service = new ArticleService({articleRepository: spyRepo});
+        
+        spyRepo.findById.and.returnValue(Promise.resolve(article));
     });
     describe('findById method', () => {
-        this.spyRepo.findById.and.returnValue(Promise.resolve(this.article));
+        
+        
         it('Should call with the correct id and return the correct article', () => {
-            this.service.findById(this.article._id).then(art => {
-                expect(art._id).toEqual(this.article._id);
+            service.findById(article._id).then(art => {
+                expect(art._id).toEqual(article._id);
             });
-            expect(this.spyRepo.findById).toHaveBeenCalledWith(this.article._id);
+            expect(spyRepo.findById).toHaveBeenCalledWith(article._id);
         })
     })
 })
