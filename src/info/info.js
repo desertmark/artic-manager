8982c7d538d508b674d6c44f178dbe69c782f4f5
Fernando Asulay <fernando@kelsus.com>
2018-12-24 15:44:24 -0300

    Show version in /info path

--- src/info/info.js
+++ src/info/info.js
@@ -1,6 +1,6 @@
 const PORT_NUMBER = process.env.PORT || 3001 ;
 const config      = require('../config/config.js');
-const package     = require('./package.json');
+const package     = require('../../package.json');
 
 module.exports = Promise.resolve({
         status: `App is running on port: ${PORT_NUMBER}`,
