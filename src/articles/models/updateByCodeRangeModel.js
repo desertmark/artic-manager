80a8754c21ac3a8061abf515104a7f869480aada
Fernando Asulay <fer.guitar.ln@gmail.com>
2019-06-09 20:06:42 -0300

    Fix validations

--- src/articles/models/updateByCodeRangeModel.js
+++ src/articles/models/updateByCodeRangeModel.js
@@ -1,23 +1,27 @@
 const { parseCode } = require('../articles-utils');
 const { ValidationError } = require('../../util/errors');
+const { isNumber } = require('lodash');
 module.exports = class UpdateByCodeRangeModel {
     constructor(model = {}) {
         try {
             this.from = parseCode(model.from);
             this.to = parseCode(model.to);
-            this.fields = model.fields;
-            if(!this.isValid(model)) {
-                throw new ValidationError('Invalid Model. Please specify from, to and what fields to update along with the values.');
-            }
+            this.fields = model.fields || {};
         } catch(error) {
             throw new ValidationError('Invalid "from" and "to" codes.', 422, error);
         }
+        if(!this.isValid(model)) {
+            throw new ValidationError('Invalid Model. Please specify from, to and what fields to update along with the values.');
+        }
     }
 
-    isValid(model) {
-        return model.fields
-            && Number.isInteger(model.from)
-            && Number.isInteger(model.to)
-            && typeof model.fields === 'object'
+    isValid() {
+        const { transport, vat, card, utility } = this.fields;
+        return (!transport || isNumber(transport))
+            && (!vat || isNumber(vat))
+            && (!card || isNumber(card))
+            && (!utility || isNumber(utility))
+            && Number.isInteger(this.from)
+            && Number.isInteger(this.to)
     }
 }
