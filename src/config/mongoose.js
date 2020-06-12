240b5dbf6063f5e15fa57b8f1aacd647bc5b9ecf
Fernando Asulay <fer.guitar.ln@gmail.com>
2020-06-12 19:07:58 +0200

    db url env

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,9 +1,10 @@
 const config = require('./config');
 const mongoose = require('mongoose');
+const conn = process.env.DATABASE_URL || config.dbConnection;
 module.exports = function initMongoose() {
-    return mongoose.connect(config.dbConnection)
-        .then(() => console.log('Mongoose Connected to', config.dbConnection))
+    return mongoose.connect(conn)
+        .then(() => console.log('Mongoose Connected to', conn))
         .catch(error => {
-            console.error(`Error while connecting mongoose to the database using ${config.dbConnection}. Reason: `, error);
+            console.error(`Error while connecting mongoose to the database using ${conn}. Reason: `, error);
         });
 }
